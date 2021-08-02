83eeda40eaf3d3a5b01c82b1b4d73bf82dc5dcb1
XuTongle <xutongle@msn.com>
2021-08-02 14:19:10 +0800

    init

---
+++ src/Traits/HasStrategies.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Larva\ThinkQcs\Traits;
+
+use Larva\ThinkQcs\Exceptions\StrategyNotFoundException;
+
+trait HasStrategies
+{
+    protected array $strategies = [];
+
+    /**
+     * @throws StrategyNotFoundException
+     */
+    public function satisfiesStrategy(array $result, string $strategy): bool
+    {
+        return !!$this->getStrategy($strategy)($result);
+    }
+
+    public function setStrategy(string $name, callable $callback): self
+    {
+        $this->strategies[$name] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * @throws StrategyNotFoundException
+     */
+    public function getStrategy(string $name): callable
+    {
+        if (!$this->hasStrategy($name)) {
+            throw new StrategyNotFoundException($name);
+        }
+
+        return $this->strategies[$name];
+    }
+
+    public function hasStrategy(string $name): bool
+    {
+        return \array_key_exists($name, $this->strategies);
+    }
+}
